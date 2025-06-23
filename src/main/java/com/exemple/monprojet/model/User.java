22d928d3a227d34decad6e17b99ec4d6300cd310
NoahRDN <noahmichaelrandrianah@gmail.com>
2025-06-23 12:51:20 +0300

    first commit

---
+++ src/main/java/com/exemple/monprojet/model/User.java
@@ -0,0 +1,19 @@
+package com.exemple.monprojet.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String email;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
